7b445ed4c4e07dd88b4333ba44102ccf1260775e
sStellar <rexstellar.pa@gmail.com>
2020-02-24 13:02:52 +0100

    Thread for new files and mod times, saving and reading data to/from data.yaml

--- FileObserver.rb
+++ FileObserver.rb
@@ -1,10 +1,13 @@
+require 'yaml'
 require_relative 'print'
+
 class FileObserver
 
   def initialize
     @all_files = []
     @mod_times = {}
     @edited_files = []
+    @yaml_data = {}
   end
 
   def getFiles()
@@ -53,6 +56,57 @@ class FileObserver
       "refresh (r) - Refreshes files and modified times, doesn't print anything"]
   end
 
+  def readData()
+    @yaml_data = YAML.load(File.read('data.yaml'))
+  end
+
+  def saveData()
+    file_data = @all_files
+    mod_data = @mod_times
+    edited_data = @edited_files
+    save_data = {
+      "file_data": file_data,
+      "mod_data": mod_data,
+      "edited_data": edited_data
+    }
+    temp_list = {"te"=>["a","bb","ccc","dddd",["eeeee","ffffff"],"ggggggg"], "st"=>["a","bb",{"test"=>"2"}]}
+    File.open("data.yaml", "w") { |file| file.write(save_data.to_yaml) }
+  end
+
+  def allFilesThread()
+    Thread.new do
+      puts "Files inventory thread started."
+      while true
+        puts getFiles()
+        sleep(3)
+      end
+    end
+  end
+
+  def modTimesThread()
+    Thread.new do
+      puts "Mod times thread started."
+      while true
+        getModTimes()
+        sleep(3)
+      end
+    end
+  end
+
+  def editedThread()
+    Thread.new do
+      puts "Edited files thread started."
+      edited_files = []
+      while true
+        getEdited()
+        edited_files = @edited_files
+        if edited_files != []
+          p edited_files
+        end
+        sleep (3)
+      end
+    end
+  end
 end
 
 x = FileObserver.new
@@ -60,8 +114,18 @@ x.helpMenu
 while true
   input = gets.chomp
   case input
+  when "save", "s"
+    x.saveData
+  when "read", "re"
+    print_h x.readData
   when "all", "a"
-    puts x.getFiles()
+    Thread.new do
+      puts "Files inventory thread started."
+      while true
+        puts x.getFiles()
+        sleep(3)
+      end
+    end
   when "refresh", "r"
     x.refreshFiles()
   when "modified", "m"
