b8a464481d374312431b59291b1499d4ca8bdb69
sStellar <rexstellar.pa@gmail.com>
2020-04-24 15:02:52 +0200

    sak

--- src/FileObserver.rb
+++ src/FileObserver.rb
@@ -9,15 +9,22 @@ class FileObserver
     @edited_files = []
     @yaml_data = {}
     @spec_dir = spec_dir
+    @new_files = []
+    @del_files = []
+
+    @pwd = Dir.pwd.chomp("src")
   end
   attr_reader :all_files
   attr_reader :mod_times
   attr_reader :edited_files
+  attr_reader :new_files
+  attr_reader :del_files
 
   def getFiles()
     @all_files = []
     Dir.glob(@spec_dir).each do |cur_file|
       if File.file?(cur_file)
+        cur_file.slice!("../")
         @all_files << cur_file
       end
     end
@@ -26,7 +33,7 @@ class FileObserver
 
   def getModTimes()
     @all_files.each do |cur_file|
-      @mod_times[cur_file] = File.mtime(cur_file)
+      @mod_times[cur_file] = File.mtime( @pwd + cur_file )
     end
     return @mod_times
   end
@@ -34,7 +41,7 @@ class FileObserver
   def getEdited()
     temp_edited = []
     @mod_times.each do |cur_file, cur_mod|
-      if File.mtime(cur_file) > cur_mod
+      if File.mtime(@pwd + cur_file) > cur_mod
         temp_edited << cur_file
       end
     end
@@ -51,8 +58,80 @@ class FileObserver
     getModTimes
   end
 
-  def readData()
-    @yaml_data = YAML.load(File.read('../data/data.yaml'))
+  def thread(files)
+    t = Thread.new do
+      new_file_inv = []
+      n_f_v = Dir.glob(@spec_dir)
+      n_f_v.each do |cur_file|
+        if File.file?(cur_file)
+          new_file_inv << cur_file
+        end
+      end
+
+
+      i = 0 # fileob all files index
+      sub_i = 0 # new files index
+      p files
+      # new files loop
+      while i < files.length
+        while sub_i < new_file_inv.length
+          if !files[i].include?(new_file_inv[sub_i])
+            # if file ob's all files does not include new files inv current file
+            if !@new_files.include?(new_file_inv[sub_i])
+              @new_files << new_file_inv[sub_i]
+            end
+            # add the new files inv current file to new files
+          end
+          sub_i += 1
+        end
+        i += 1
+      end
+
+
+
+
+      #del files loop
+      i = 0
+      sub_i = 0
+      while i < new_file_inv.length
+        while sub_i < files.length
+          if !new_file_inv[i].include?(files[sub_i])
+            # if new file invs current file does not include file obs current file
+            if !@del_files.include?(files[sub_i])
+              @del_files << files[sub_i]
+            end
+            # add the deleted file to del files
+            # this works becuase new files inv is updated when the program starts
+            # but file obs inv is outdated, if file is deleted, file obs inv still has it but not new files inv
+          end
+          sub_i += 1
+        end
+        i += 1
+      end
+=begin
+
+      puts "_______________________________________________________"
+
+      puts "mod times:"
+      print_h getModTimes()
+      puts "edited:"
+      p getEdited()
+      puts "nfv:"
+      p new_file_inv
+      puts "new:"
+      p @new_files
+      puts "del:"
+      p @del_files
+=end
+    end
+    t.join
+  end
+  def readData(dir = '../data/data.yaml')
+    @yaml_data = YAML.load(File.read(dir))
+
+    @all_files = @yaml_data[:file_data]
+    @mod_times = @yaml_data[:mod_data]
+    @edited_files = @yaml_data[:edited_data]
   end
 
   def saveData()
--- src/gui.rb
+++ src/gui.rb
@@ -8,90 +8,73 @@ class Gui < FXMainWindow
   def initialize(main_app, s_d = "../**/*")
     @spec_dir = s_d
     @fileob = FileObserver.new(@spec_dir)
-    @fileob.readData()
-    @new_files = []
-    @del_files = []
+    p Dir.pwd
+    @fileob.readData("/home/peter/Sync/data/data.yaml")
 
-    main_window = super(main_app, "File Sync", :width => 200, :height => 100)
+    # ----------------------- TEXT START ------------------------------ #
+    super(main_app, "File Sync", :width => 200, :height => 100)
+
+    # --------------------------- FRAME SETUP ---------------------------- #
 
     v_frame = FXVerticalFrame.new( self, :opts => LAYOUT_FILL )
-    h_frame = FXHorizontalFrame.new(v_frame, :padding => 0 )
+    h_frame = FXHorizontalFrame.new( v_frame, :padding => 0 )
     matrix = FXMatrix.new( h_frame, 3, MATRIX_BY_COLUMNS|LAYOUT_FILL )
+
+    # ------------------------ FILES TABLE --------------------------- #
+
+    FXLabel.new(matrix, "Treelist:")
+    @fileob.thread( @fileob.all_files )
+
+    @treelist = FXTreeList.new(v_frame,:opts => TREELIST_NORMAL|TREELIST_SHOWS_LINES|TREELIST_SHOWS_BOXES|TREELIST_ROOT_BOXES|LAYOUT_FILL)
+
+
+    
+=begin
+    listview_frame = FXHorizontalFrame.new(v_frame, :opts => FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL|LAYOUT_TOP, :padding => 0)
+    @table = FXTable.new(listview_frame, :opts => LAYOUT_FILL)
+    @table.tableStyle |= TABLE_COL_SIZABLE
+
+    @table.rowHeaderWidth = 0
+    @table.columnHeaderMode = LAYOUT_FIX_HEIGHT
+    @table.columnHeaderHeight = 0
+    @table.columnHeader
+
+    filefuck = { "Jane Doe" => 10, "Jim Do" => 6, "Jim oe" => 6, "Jim De" => 6, "Ji Doe" => 6 }
+    a = 1
+    b = 0
+    wd = Dir.pwd
+    dir = Dir.glob('**/*', base:wd)
+    c = dir.length
+
+    @table.setTableSize(c + 1, 4)
+    @table.setItemText(0,0, "Contents of Keylocker folder")
+    @table.setItemJustify(0,0, FXTableItem::CENTER_X|FXTableItem::CENTER_Y)
+
+    dir.each do |dir|
+        temp = File.mtime(dir)
+        @table.setItemText(a, b, "#{wd}/#{dir}")
+        @table.setItemJustify(a, 0, FXTableItem::LEFT)
+        @table.setItemText(a, 3, "#{temp}")
+        a += 1
+    end
+=end
+
   end
+
 
   # Create App
   def create()
     super
     show(PLACEMENT_SCREEN)
   end
-
-  def thread()
-    t = Thread.new do
-      new_file_inv = []
-      n_f_v = Dir.glob(@spec_dir)
-      n_f_v.each do |cur_file|
-        if File.file?(cur_file)
-          new_file_inv << cur_file
-        end
-      end
-      i = 0 # fileob all files index
-      sub_i = 0 # new files index
-
-      # new files loop
-      while i < @fileob.all_files.length
-        while sub_i < new_file_inv.length
-          if !@fileob.all_files[i].include?(new_file_inv[sub_i])
-            # if file ob's all files does not include new files inv current file
-            if !@new_files.include?(new_file_inv[sub_i])
-              @new_files << new_file_inv[sub_i]
-            end
-            # add the new files inv current file to new files
-          end
-          sub_i += 1
-        end
-        i += 1
-      end
-
-      #del files loop
-      i = 0
-      sub_i = 0
-      while i < new_file_inv.length
-        while sub_i < @fileob.all_files.length
-          if !new_file_inv[i].include?(@fileob.all_files[sub_i])
-            # if new file invs current file does not include file obs current file
-            if !@del_files.include?(@fileob.all_files[sub_i])
-              @del_files << @fileob.all_files[sub_i]
-            end
-            # add the deleted file to del files
-            # this works becuase new files inv is updated when the program starts
-            # but file obs inv is outdated, if file is deleted, file obs inv still has it but not new files inv
-          end
-          sub_i += 1
-        end
-        i += 1
-      end
-
-      puts "mod times:"
-      print_h @fileob.getModTimes()
-      puts "edited:"
-      p @fileob.getEdited
-      puts "nfv:"
-      p new_file_inv
-      puts "new:"
-      p @new_files
-      puts "del:"
-      p @del_files
-    end
-    t.join
-  end
 end
 
 
 if __FILE__ == $0
-    FXApp.new do |main_app|
-        Gui.new(main_app)
+  FXApp.new do |main_app|
+    gui = Gui.new(main_app)
 
-        main_app.create
-        main_app.run
-    end
+    main_app.create
+    main_app.run
+  end
 end
