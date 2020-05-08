ddf901373faf071f89ba5d620050d057eaeec885
sStellar <rexstellar.pa@gmail.com>
2020-05-08 16:45:55 +0200

    Changed frame layout, new files table and frames added. Tables.rb created, ev. move table functions to Tables.rb

--- gui.rb
+++ gui.rb
@@ -33,54 +33,73 @@ class Gui < FXMainWindow
     end
     # logic_t.join
 
-    # ----------------------- TEXT START ------------------------------ #
-    super(main_app, "File Sync", :width => 200, :height => 100)
+    # ----------------------- APP START ------------------------------ #
+    super(main_app, "File Sync", :width => 600, :height => 400)
 
     # --------------------------- FILES FRAME SETUP ---------------------------- #
 
-    v_frame = FXVerticalFrame.new( self, :opts => LAYOUT_FILL )
-    h_frame = FXHorizontalFrame.new( v_frame, :padding => 0 )
-    matrix = FXMatrix.new( h_frame, 3, MATRIX_BY_COLUMNS|LAYOUT_FILL )
+    main_frame = FXVerticalFrame.new( self, :opts => LAYOUT_FILL )
+    #h_frame = FXHorizontalFrame.new( v_frame, :padding => 0 )
+    files_matrix = FXMatrix.new( main_frame, 3, MATRIX_BY_COLUMNS )
+    FXLabel.new(files_matrix, "Files list: #{@spec_dir}")
+
+    new_matrix = FXMatrix.new( main_frame, 3, MATRIX_BY_COLUMNS )
+    FXLabel.new(new_matrix, "New Files:")
+
+
+    main_table_frame = FXHorizontalFrame.new(main_frame, :padding => 0, :opts => LAYOUT_FILL)
+
 
     # ------------------------ FILES TABLE --------------------------- #
 
-    FXLabel.new(matrix, "Files list: #{@spec_dir}")
+    file_list_frame = FXVerticalFrame.new(main_table_frame, :opts => FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL|LAYOUT_TOP, :padding => 0)
+    files_table = FXTable.new(file_list_frame, :opts => LAYOUT_FILL, :padLeft => 7, :padRight => 10)
+    files_table.tableStyle |= TABLE_COL_SIZABLE
+    files_table.rowHeaderWidth = 5
+    files_table.columnHeaderMode = LAYOUT_FIX_HEIGHT
+    files_table.columnHeaderHeight = 10
+    files_table.columnHeader
 
-    file_list_frame = FXHorizontalFrame.new(v_frame, :opts => FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL|LAYOUT_TOP, :padding => 0)
-    @table = FXTable.new(file_list_frame, :opts => LAYOUT_FILL, :padLeft => 7, :padRight => 10)
-    @table.tableStyle |= TABLE_COL_SIZABLE
-    @table.rowHeaderWidth = 5
-    @table.columnHeaderMode = LAYOUT_FIX_HEIGHT
-    @table.columnHeaderHeight = 10
-    @table.columnHeader
 
-    # ------- Add file inv text to table --------
+    # ------------------- NEW FILES TABLE ---------------------
 
-    files_running = true
-    files_t = Thread.new do
-      while files_running
-        @table.setTableSize(@fileob.file_inv.length+1, 2)
-        @table.setItemText(0, 0, "Files list:")
-        @table.setItemText(0, 1, "#{@fileob.file_inv.length} files")
-        @table.setItemJustify(0, 0, FXTableItem::CENTER_X|FXTableItem::CENTER_Y)
-        @table.setItemJustify(0, 1, FXTableItem::LEFT)
+    new_table = FXTable.new(new_list_frame, :opts => LAYOUT_FILL, :padLeft => 7, :padRight => 10)
+    new_table.tableStyle |= TABLE_COL_SIZABLE
+    new_table.rowHeaderWidth = 5
+    new_table.columnHeaderMode = LAYOUT_FIX_HEIGHT
+    new_table.columnHeaderHeight = 10
+    new_table.columnHeader
+
+    # ------------------- TABLES THREAD AND LOOP -----------------------
+
+    tables_running = true
+    tables_t = Thread.new do
+      while tables_running
+        # --------------- ALL FILES TABLE ---------------
+        files_table.setTableSize(@fileob.file_inv.length+1, 2)
+        files_table.setItemText(0, 0, "Files list:")
+        files_table.setItemText(0, 1, "#{@fileob.file_inv.length} files")
+        files_table.setItemJustify(0, 0, FXTableItem::CENTER_X|FXTableItem::CENTER_Y)
+        files_table.setItemJustify(0, 1, FXTableItem::LEFT)
 
         v_i = 1 # Row text is displayed on, row 0 is "Files list"
         @fileob.file_inv.each do |file, mtime|
           file = file.include?(@spec_dir) ? file.sub(@spec_dir+"/","") : file # Remove directory from file names
           # If file has spec dir in it, file becomes file without spec dir. If it doesnt, just return file
           # Not sure when this would be useful but it could be, and it works and was fun so....
-          @table.setItemText(v_i, 0, file)
-          @table.setItemJustify(v_i, 0, FXTableItem::LEFT)
-          @table.setItemText(v_i, 1, mtime.to_s)
+          files_table.setItemText(v_i, 0, file)
+          files_table.setItemJustify(v_i, 0, FXTableItem::LEFT)
+          files_table.setItemText(v_i, 1, mtime.to_s)
 
           v_i += 1
         end
-        @table.fitColumnsToContents(0)
-        @table.fitColumnsToContents(1)
+        files_table.fitColumnsToContents(0)
+        files_table.fitColumnsToContents(1)
         sleep(1)
       end
     end
+
+    new_list_frame = FXVerticalFrame.new(main_table_frame, :opts => FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL|LAYOUT_TOP, :padding => 0)
 
   end
 
---
+++ src/Tables.rb
@@ -0,0 +1,5 @@
+require "fox16"
+
+class Tables
+
+end
